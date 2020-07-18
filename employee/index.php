a0f6094ba2af38f0e6a36c456c2446844e35dd51
Gabriel V. Agoncillo <gabrielvagoncillo@pop-os.localdomain>
2020-07-18 15:54:08 +0800

    Preparing dashboard for employees

---
+++ employee/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Haier Online Payslip</title>
+
+     <!-- Compiled and minified CSS -->
+     <link rel="stylesheet" type="text/css" href="">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body>
+
+<nav class="transparent">
+    <div class="nav-wrapper">
+        <div class="container">
+
+            <a href="#" class="brand-logo">
+            	<img src="../images/LOGO2-01.png" alt="" class="responsive-img" style="height: 30px;" >
+            </a>
+            <!-- <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+            </ul> -->
+            </div>
+
+        </div>
+  </nav>
+
+
+
+	<!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+</body>
+</html>
