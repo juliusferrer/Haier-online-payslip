1ee8f189445b199412342879da2d12322cdb2e72
Julius Ferrer <53897181+juliusferrer@users.noreply.github.com>
2020-07-25 16:43:31 +0800

    Update payslip_details.php

--- employee/payslip_details.php
+++ employee/payslip_details.php
@@ -10,6 +10,8 @@
      <link rel="stylesheet" type="text/css" href="">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
+
+
 <body>
 
 <nav class="transparent">
@@ -45,8 +47,238 @@
    
 <br>  
    
-  
+  <!-- <section>
+        <div class="container grey lighten-5">
 
+        <table class="highlight responsive-table centered z-depth-4">
+        <thead class="light-blue darken-5"> 
+          <tr class="white-text ">
+              <th>Pay Period</th>
+              <th>Action</th>
+              <th>Net Pay</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>07/01/2020 - 07/15/2020</td>
+            <td><a href="">View Details</a></td>
+            <td>25,000</td>
+          </tr>
+          <tr>
+            <td>07/16/2020 - 07/31/2020</td>
+            <td><a href="">View Details</a></td>
+            <td>25,000</td>
+          </tr>
+          <tr>
+            <td>08/01/2020 - 01/15/2020</td>
+            <td><a href="">View Details</a></td>
+            <td>25,000</td>
+          </tr>
+        </tbody>
+      </table>
+
+        </div>
+  </section> -->
+
+  
+	
+    <section>
+
+    <div class="container grey lighten-5 z-depth-2">
+   
+  
+    <div class="table-responsive">
+    
+
+        <div class="row">
+
+        <div class="col m6 s12">
+        <table class="highlight responsive-table">
+      
+            <tbody>
+           <p>  HAIER ELECTRICAL APPLIANCES PHILIPPINES, INC</p> 
+            <tr>
+                <th>Department:</th>
+                <td>Marketing</td>
+              </tr>
+
+              <tr>
+                <th>Employee Name:</th>
+                <td>DELA CRUZ, JUAN M. - 0357</td>
+              </tr>
+
+              <tr>
+                <th> Basic Pay</th>
+                <td>P8166.88</td>
+              </tr>
+
+              <tr>
+                 <th>Overtime Pay</th>
+                <td>671.25</td>
+              </tr>
+              
+              <tr>
+                 <th>INCENTIVES</th>
+                <td>1,502.2</td>
+              </tr>
+
+              
+            </tbody>
+
+        </table>
+        
+        </div>
+
+        <div class="col m6 s12">
+        <table class="highlight responsive-table">
+
+            <tbody>
+            <p>  Proof Sheet - Trial Computation</p> 
+            <tr>
+                <th>July - Pay Period</th>
+                <td>07/01/2020 to 07/15/2020</td>
+              </tr>
+              <tr>
+                <th>SSS Contribution</th>
+                <td>420.00</td>
+              </tr>
+
+              <tr>
+                <th>PHIC Contribution</th>
+                <td>150.00</td>
+              </tr>
+
+              <tr>
+                <th>HDMF Contribution</th>
+                <td>100.00</td>
+              </tr>
+
+              <tr>
+                <th>SSS LOAN</th>
+                <td>0.00</td>
+              </tr>
+
+              <tr>
+                <th>PAG-IBIG LOAN</th>
+                <td>475.00</td>
+              </tr>
+
+            
+            </tbody>
+        </table>
+        
+        </div>
+        <br>
+        <div class="fluid">
+        <div class="row">
+        <div class="col s12">
+        <br>
+        <div class="divider"></div>
+        </div>
+        </div>
+        </div>
+
+
+        <div class="col m6 s12">
+        <table class="highlight responsive-table"> 
+          
+            <tr class="total">
+                <th>Total Income:</th>
+                <td>10,340.84</td>
+              </tr>
+
+              <tr>
+                 <th>Undertime/Late</th>
+                <td>0:00</td>
+              </tr>
+              
+
+              <tr>
+                 <th>OTRegular</th>
+                <td>0:00</td>
+              </tr>
+
+              <tr>
+                 <th>Night Diff.</th>
+                <td>0:00</td>
+              </tr>
+            
+                  
+            
+            </tfoot>
+        </table>
+
+        </div>
+
+        <div class="col m6 s12">
+        <table class="highlight responsive-table"> 
+          
+            <tr class="total">
+                <th>Total Deduction:</th>
+                <td>1,145.00</td>
+              </tr>
+
+              <tr>
+                 <th>Net Pay:</th>
+                <td>9,195</td>
+              </tr>
+              
+
+              <tr>
+                 <th>OT SUN:</th>
+                <td>0:00</td>
+              </tr>
+
+              <tr>
+                 <th>OT LEGAL HOLIDAY:</th>
+                <td>0:00</td>
+              </tr>
+
+              <tr>
+                 <th>OT SPECIAL HOLIDAY:</th>
+                <td>0:00</td>
+              </tr>      
+            
+            </tfoot>
+        </table>
+        </div>
+        
+
+        <!-- <div class="col s12">
+            <table>
+            <tr>
+            <td><a href="">View Details</a></td>
+            </tr>
+        </table>
+        </div> -->
+       
+        <!-- <div class="fluid">
+            <div class="row">
+                 <div class="col s12">
+
+                
+            <br>
+            <br>
+                 <div class="divider"></div>
+                </div>
+             </div>
+        </div> -->
+      
+    </div>
+
+</div>
+
+        
+    </div>
+    </div>
+    <div class="container">
+        <button class="btn blue darken-4 waves-effect waves-dark" style="width:100%;">PRINT / VIEW PAYSLIP</button>
+    
+    </div>
+
+    </section>
+  
 
 
 	<!-- Compiled and minified JavaScript -->
