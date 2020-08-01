a5c32db0fbdbbf2f3c03e242b2200f14f08c2dc1
Julius Ferrer <53897181+juliusferrer@users.noreply.github.com>
2020-08-01 14:13:36 +0800

    Update connect.php
    connection

--- connect.php
+++ connect.php
@@ -8,12 +8,17 @@ $dbname = "online_payroll";
 $mysqli = new mysqli($servername, $username, $password, $dbname);
 
  # Code block if the database is connected
-	// if($mysqli -> connect_errno){
-	// 	echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
- //  exit();
-	// }
-	// else {
-	// 	echo "Database Connected";
-	// }
+	if($mysqli -> connect_errno){
+		echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+	}
+	else {
+		echo "Database Connected";
+	}
+
+	// Summary PAyslip 
+	$sql = "SELECT * FROM employee_record";
+	$queryEmployeeRecord = $mysqli->query($sql) or die($con->error);
+	$row = $queryEmployeeRecord->fetch_assoc();
 
 ?>
