182cefcf6982d2161417530bb619832a629eeae6
Julius Ferrer <53897181+juliusferrer@users.noreply.github.com>
2020-08-01 14:53:16 +0800

    Update view_summary.php
    same

--- employee/view_summary.php
+++ employee/view_summary.php
@@ -3,7 +3,7 @@ include("../connect.php");
   
   $sqlrecord_employee = "SELECT * FROM employee_record"  or die (mysqli_error());
   $queryEmployeeRecord = $mysqli->query($sqlrecord_employee) or die($mysqli->error);
-$row = $queryEmployeeRecord->fetch_assoc();
+  $row = $queryEmployeeRecord->fetch_assoc();
 
 // do{
 //   echo $row['employee_id']." ".$row['payPeriod']." ".$row['netPay']. "<br/>";
